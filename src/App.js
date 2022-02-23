d5f48ed4207181191ba572ade03a838734d1e9ff
Victor Farina <victor.farina@mail.com>
2022-02-23 16:10:08 -0400

    adding ShopData component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
-import './App.css';
-import { Switch, Route } from 'react-router-dom';
+import "./App.css"
+import { Switch, Route } from "react-router-dom"
 
-import Homepage from './pages/homepage/homepage.component.jsx';
-import ShopData from './pages/shop/shop.component.jsx'
+import Homepage from "./pages/homepage/homepage.component.jsx"
+import ShopData from "./pages/shop/shop.component.jsx"
 
 function App() {
-
   return (
     <div>
-        <Switch>
-          <Route exact path='/' component={Homepage}/>
-          <Route exact path='/hats' component={ShopData}/>
-        </Switch>
+      <Switch>
+        <Route exact path='/' component={Homepage} />
+        <Route exact path='/hats' component={ShopData} />
+      </Switch>
     </div>
-
   )
-
 }
 
-export default App;
+export default App
