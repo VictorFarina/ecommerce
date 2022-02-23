759e19c13efc08b019bd667ade1715f44d9103ba
Victor Farina <victor.farina@mail.com>
2022-02-23 15:39:14 -0400

    updating menu-item withRouter

--- src/App.js
+++ src/App.js
@@ -4,14 +4,19 @@ import { Switch, Route } from 'react-router-dom';
 import Homepage from './pages/homepage/homepage.component.jsx'
 function App() {
 
-  
+  const testComponent = () =>(
+    <div>TEST</div>
+  )
+
   return (
 
     <div>
         <Switch>
           <Route exact path='/' component={Homepage}/>
+          <Route exact path='/hats' component={testComponent}/>
         </Switch>
     </div>
+
   )
 
 }
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -3,10 +3,9 @@ import { withRouter } from 'react-router-dom';
 
 import './menu-item.styles.scss';
 
-const MenuItem = ({ title, imageUrl, size, history ,linkUrl }) => (
+const MenuItem = ({ title, imageUrl, size, history ,linkUrl, match }) => (
 
-  <div className={`${size} menu-item`} onClick={() => history.push(linkUrl)}>
-    {console.log('detta är vad som loggas:'+linkUrl)}
+  <div className={`${size} menu-item`} onClick={() => history.push(`${match.url}${linkUrl}`)}>
     <div
       className='background-image'
       style = {{
@@ -18,7 +17,6 @@ const MenuItem = ({ title, imageUrl, size, history ,linkUrl }) => (
       <span className='subtitle'>SHOP NOW</span>
     </div>
   </div>
-
 )
 
 export default withRouter (MenuItem);
