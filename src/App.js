ac84cb3118f15923c30cc11ac027031f153e5a1a
Victor Farina <victor.farina@mail.com>
2022-02-23 14:59:12 -0400

    passing linkUrls

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import { Switch, Route } from 'react-router-dom';
 
 import Homepage from './pages/homepage/homepage.component.jsx'
 function App() {
+
   
   return (
+
     <div>
         <Switch>
           <Route exact path='/' component={Homepage}/>
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 
 import './menu-item.styles.scss';
 
-const MenuItem = ({ title, imgUrl, size }) => (
-  <div className={`${size} menu-item`}>
+const MenuItem = ({ title, imageUrl, size, history ,linkUrl }) => (
+
+  <div className={`${size} menu-item`} onClick={() => history.push(linkUrl)}>
+    {console.log('detta är vad som loggas:'+linkUrl)}
     <div
       className='background-image'
-      style={{
-        backgroundImage: `url(${imgUrl})`
+      style = {{
+        backgroundImage: `url(${imageUrl})`
       }}
     />
     <div className='content'>
@@ -15,6 +18,7 @@ const MenuItem = ({ title, imgUrl, size }) => (
       <span className='subtitle'>SHOP NOW</span>
     </div>
   </div>
-);
 
-export default MenuItem;
+)
+
+export default withRouter (MenuItem);
